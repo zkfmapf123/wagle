4d5d7fad7b76ade9f12ec97e679ba4175d4a9ae2
leedonggyu <zkfmapf123@naver.com>
2021-03-01 00:14:44 +0900

    12

--- src/Screens/App.js
+++ src/Screens/App.js
@@ -1,27 +1,46 @@
 import React from "react";
+import { useState } from "react";
+import { useEffect } from "react";
 import './App.css';
 import Footer from './Footers/Footer';
 import Header from './Headers/Header';
 import Main from './Mains/Main';
 
 function App() {
-  return (
-    <div className="container">
-      <div className="header">
-        <Header 
-              onPressDescription={"#description"}
-              onPressList={"#list"}
-              onPressLocation={"#location"}/>
-              
+  const [loading, setLoading] = useState(true);
+
+  useEffect(()=>{
+    setTimeout(()=>{
+      setLoading(false);
+    },3000);
+  },[]);
+
+  if(loading){
+    return(
+      <div className="login-container">
+        <h1>와글와글 공방에 오신걸 환영합니다</h1>
+        <h3>조금만 기다려주세요</h3>
       </div>
-      <div className="main">
-        <Main/>
+    )
+  }else{
+    return (
+      <div className="container">
+        <div className="header">
+          <Header 
+                onPressDescription={"#description"}
+                onPressList={"#list"}
+                onPressLocation={"#location"}/>
+                
+        </div>
+        <div className="main">
+          <Main/>
+        </div>
+        <div id="location" className="footer">
+          <Footer/>
+        </div>
       </div>
-      <div id="location" className="footer">
-        <Footer/>
-      </div>
-    </div>
-  );
+    );
+  }
 }
 
 export default App;
